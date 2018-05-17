c6367e037167b8e05a91790b56fc1932da4dfe0b
INFOR\yli5 <yuan.li@infor.com>
2018-05-17 14:44:20 -0700

    create Recipes component and display it

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe'
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+
+
+  recipe : Recipe={
+    id:1,
+    name: 'Kung Pao Chicken'
+  }
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
