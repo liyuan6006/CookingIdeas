11fcf98d50c41c615542058633836ec3e0619a7a
INFOR\yli5 <yuan.li@infor.com>
2018-05-19 14:41:40 -0700

    Add routing to recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import {FormsModule} from '@angular/forms';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { MessagesComponent } from './messages/messages.component'
+import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module'
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MessagesComponent } from './messages/messages.component'
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
